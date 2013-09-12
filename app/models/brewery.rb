1c43d8fd9ee39a67f4872451eee4568257813b71
rikniitt <riku.niittymaki@helsinki.fi>
2013-09-12 13:23:28 +0300

    week3. ex1

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,7 +2,15 @@ class Brewery < ActiveRecord::Base
 	attr_accessible :name, :year
 
 	has_many :beers
+	has_many :ratings, :through => :beers
 	
+	def average_rating
+		if not ratings.empty?
+			ratings.inject(0.0) { |sum, rating| sum + rating.score } / ratings.size
+		else
+			0.0
+		end
+	end
 	
 	def to_s
 		name
