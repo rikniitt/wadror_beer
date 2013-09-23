e8d2dd4d359b5714707961e49a4c59b355f26439
rikniitt <riku.niittymaki@helsinki.fi>
2013-09-23 21:59:40 +0300

    ex11

--- Gemfile
+++ Gemfile
@@ -57,6 +57,7 @@ group :test do
   gem 'factory_girl_rails'
   gem 'capybara'
   gem 'launchy'
+  gem 'simplecov', :require => false
 end
 
 group :production do
--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Brewery do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it "has the name and year set correctly and is saved to database" do
+		brewery = Brewery.create :name => "Schlenkerla", :year => 1674
+
+		brewery.name.should == "Schlenkerla"
+		brewery.year.should == 1674
+		brewery.should be_valid
+	end
+  
+	it "without a name is not valid" do
+		brewery = Brewery.create  :year => 1674
+	
+		brewery.should_not be_valid
+	end
+
+	describe "when initialized with name Schlenkerla and year 1674" do
+	  subject{ Brewery.create :name => "Schlenkerla", :year => 1674 }
+
+	  it { should be_valid }
+	  its(:name) { should eq("Schlenkerla") }
+	  its(:year) { should eq(1674) }
+	end
 end
