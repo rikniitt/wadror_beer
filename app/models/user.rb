c0b9a34cea6049a613da37ba00499f39134a3633
rikniitt <riku.niittymaki@gmail.com>
2013-10-02 11:43:13 +0300

    fixed places spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,13 +24,38 @@ class User < ActiveRecord::Base
 		ratings.sort_by{ |r| r.score }.last.beer
 	end
 	
-	def favorite_style
-		return favorite_beer.style unless ratings.empty?
+	def favorite_brewery
+	    return nil if ratings.empty?
+		brewery_rating_pairs = rated_breweries.inject([]) do |pairs, brewery|
+			pairs << [brewery, brewery_rating_average(brewery)]
+		end
+		brewery_rating_pairs.sort_by { |s| s.last }.last.first
+	end
+	def rated_breweries
+		ratings.map{ |r|r.beer.brewery }.uniq
+	end
+	def brewery_rating_average(brewery)
+		ratings_of_brewery = ratings.select{ |r|r.beer.brewery==brewery }
+		return 0 if ratings_of_brewery.empty?
+		ratings_of_brewery.inject(0.0){ |sum ,r| sum+r.score } / ratings_of_brewery.count
 	end
 	
-	def favorite_brewery
-		return favorite_beer.brewery unless ratings.empty?
+	def favorite_style
+		return nil if ratings.empty?
+		style_rating_pairs = rated_styles.inject([]) do |pairs, style|
+			pairs << [style, style_rating_average(style)]
+		end
+		style_rating_pairs.sort_by { |s| s.last }.last.first
 	end
+	def rated_styles
+		ratings.map{ |r|r.beer.style }.uniq
+	end
+	def style_rating_average(style)
+		ratings_of_style = ratings.select{ |r| r.beer.style==style }
+		return 0 if ratings_of_style.empty?
+		ratings_of_style .inject(0.0){ |sum ,r| sum+r.score } / ratings_of_style.count
+	end
+	
 	
 	def is_admin?
 		admin == true
